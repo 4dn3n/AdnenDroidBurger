9b70e180b9abfa66bfa3cf4fd5d4f05c3e32835f
adnen <adnen-kaddouri@outlook.fr>
2023-11-25 16:01:17 +0100

    Initial commit

---
+++ app/src/main/java/fr/isen/kaddouri/droidburger/MainActivity.kt
@@ -0,0 +1,18 @@
+package fr.isen.kaddouri.droidburger
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import fr.isen.kaddouri.droidburger.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
